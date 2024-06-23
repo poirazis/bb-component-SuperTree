b7398b25c492a34444128b9f57deb9a4ad45424e
poirazis <poirazis@gmail.com>
2024-06-23 14:15:03 +0300

    wip

--- lib/Tree.svelte
+++ lib/Tree.svelte
@@ -1,93 +1,107 @@
 <script>
-  import { getContext, createEventDispatcher } from "svelte"
-  const { Provider } = getContext("sdk")
+  import { getContext, createEventDispatcher } from "svelte";
+  const { Provider } = getContext("sdk");
+  const treeOptions = getContext("superTreeOptions");
+
   const dispatch = createEventDispatcher();
 
-  export let label
-  export let children = []
-  export let quiet
-  export let open = false
-  export let nodeSelection
-  export let selectedNodes
-  export let icon
-  export let hasSlot
-  export let renderSlot
-  export let id
-  export let disabled
-
+  export let label;
+  export let children = [];
+  export let quiet;
+  export let open = false;
+  export let hasSlot;
+  export let renderSlot;
+  export let id;
+  export let disabled;
 
   $: context = {
-    nodeId : id,
-    nodeLabel : label
-  }
+    nodeId: id,
+    nodeLabel: label,
+  };
 
-  $: if ( disabled ) open = false
-  
-  const handleClick = e => {
-    if ( disabled ) return;
+  $: if (disabled) open = false;
+  $: selectionStore = $treeOptions.selectedNodes;
+  $: selected = $selectionStore.findIndex((x) => x.id == id) > -1;
+  $: icon = $treeOptions.nodeIcon;
 
-    if ( children?.length || hasSlot ) {
-      open = !open 
-      return
+  const handleClick = (e) => {
+    if (disabled) return;
+
+    if (children?.length || hasSlot) {
+      open = !open;
+      return;
+    } else if ($treeOptions.nodeSelection) {
+      dispatch("nodeSelect", { id, label });
+      return;
     }
-    else if (nodeSelection) {
-      dispatch("nodeSelect", {id, label})
-    }
-
-    dispatch("nodeClick", {id,label})
-  }
+    dispatch("nodeClick", { id, label });
+  };
 </script>
 
 <!-- svelte-ignore a11y-missing-attribute -->
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <!-- svelte-ignore a11y-no-static-element-interactions -->
-<li class="spectrum-TreeView-item"
+<li
+  class="spectrum-TreeView-item"
   class:is-disabled={disabled}
-  class:is-selected={$selectedNodes.findIndex( x => x.id == id ) > -1 }
-  class:is-open={open} >
-  <a 
+  class:is-selected={selected}
+  class:is-open={open}
+>
+  <a
     class="spectrum-TreeView-itemLink"
-    style:padding-left={ children?.length || hasSlot || quiet ? "0.25rem" : "0.5rem" }
     on:click={handleClick}
-    >
-    {#if (children?.length || (hasSlot &&  renderSlot) ) && !quiet} 
-      <i class="ri-arrow-right-s-line chevron" class:open> </i>
+    style:padding-left={children?.length ? "0.25rem" : "1.75rem"}
+  >
+    {#if children?.length}
+      <i
+        class="ri-arrow-right-s-line chevron"
+        class:open
+        class:childless={children?.length < 1}
+      >
+      </i>
+    {/if}
+
+    {#if $treeOptions?.checkboxes}
+      {#if selected}
+        <i class="ri-checkbox-line icon" />
+      {:else}
+        <i class="ri-checkbox-blank-line icon" />
+      {/if}
     {/if}
 
     {#if icon}
-      <i class={icon} class:icon/>
+      <i class={icon} class:icon />
     {/if}
 
     {label || "Not Set"}
   </a>
 
-  {#if open}
-    {#if children?.length || hasSlot}
-      <ul class="spectrum-TreeView" class:spectrum-TreeView--quiet={quiet}>
-        {#each children as node, idx }
-          <svelte:self 
-            id={node.id}
-            hasSlot={node.hasSlot}
-            renderSlot={node.renderSlot}
-            icon={node.icon}
-            nodeSelection={node.nodeSelection}
-            selectedNodes={node.selectedNodes}
-            label={node.label}
-            children={node.children}
-            quiet={node.quiet}
-            open={node.open}
-            on:nodeSelect
-            on:nodeClick
-            > 
-            <slot/>
-          </svelte:self>
-        {/each}
-      </ul>
-      {#if renderSlot}
-        <Provider data={context}>
+  {#if children?.length || hasSlot}
+    <ul
+      class="spectrum-TreeView"
+      style:display={open ? "block" : "none"}
+      class:spectrum-TreeView--quiet={quiet}
+    >
+      {#each children as node, idx}
+        <svelte:self
+          id={node.id}
+          hasSlot={node.hasSlot}
+          renderSlot={node.renderSlot}
+          label={node.label}
+          children={node.children}
+          quiet={node.quiet}
+          open={node.open}
+          on:nodeSelect
+          on:nodeClick
+        >
           <slot />
-        </Provider>
-      {/if}
+        </svelte:self>
+      {/each}
+    </ul>
+    {#if renderSlot}
+      <Provider data={context}>
+        <slot />
+      </Provider>
     {/if}
   {/if}
 </li>
@@ -106,23 +120,14 @@
     padding-left: unset;
   }
 
-  .spectrum-TreeView-itemLink:hover > i {
-    color: var(--spectrum-global-color-blue-400);
-  }
-
   .icon {
-    font-size: 16px;
-    color: var(--spectrum-global-color-gray-500);
-  }
-
-  .selected {
-    font-size: 16px;
-    color: var(--spectrum-global-color-blue-500);
+    font-size: 18px;
+    color: var(--spectrum-global-color-gray-700);
   }
   .chevron {
     transition: all 130ms;
     font-size: 16px;
-    color: var(--spectrum-global-color-gray-500);  
+    color: var(--spectrum-global-color-gray-700);
   }
 
   .chevron.open {
--- src/Component.svelte
+++ src/Component.svelte
@@ -1,287 +1,309 @@
 <script>
-  import { getContext } from "svelte"
-  import { writable } from "svelte/store"
+  import { getContext, setContext } from "svelte";
+  import { writable } from "svelte/store";
   import Tree from "../lib/Tree.svelte";
   import Skeleton from "./Skeleton.svelte";
 
-  const { styleable, ActionTypes, Provider, ContextScopes, fetchData, processStringSync, LuceneUtils, API, builderStore, notificationStore } = getContext("sdk")
-  const component = getContext("component")
+  const {
+    styleable,
+    ActionTypes,
+    Provider,
+    ContextScopes,
+    fetchData,
+    processStringSync,
+    QueryUtils,
+    memo,
+    API,
+    builderStore,
+    notificationStore,
+  } = getContext("sdk");
 
-  export let source 
-  export let customNodes
-  export let branchName
+  const component = getContext("component");
 
-  export let collapsed
-  export let quiet
-  export let disabled
-  export let rootless
-  export let rootIcon
-  export let nodeIcon
-  export let groupNodeIcon
-  export let groupNodeLabel
-  export let nodeSelection
-  export let maxNodeSelection
+  export let branchName;
+  export let treeType = "list";
 
-  export let datasource
-  export let filter = {}
-  export let sortColumn
-  export let sortOrder
-  export let limit
-  export let paginate
-  export let labelColumn
-  export let valueColumn
-  export let groupBy
-  export let recursive
-  export let joinField
-  export let groupField
-  export let linkFields
+  export let collapsed;
+  export let quiet;
+  export let disabled;
+  export let rootless;
+  export let rootIcon;
+  export let nodeIcon;
+  export let groupNodeIcon;
+  export let groupNodeLabel;
+  export let nodeSelection;
+  export let checkboxes;
+  export let selectableBranches;
+  export let maxNodeSelection;
+
+  export let datasource;
+  export let filter = {};
+  export let sortColumn;
+  export let sortOrder;
+  export let limit;
+  export let paginate;
+  export let labelColumn;
+  export let valueColumn;
+
+  export let joinField;
+  export let groupField;
+  export let linkFields;
 
   // Events
-  export let onNodeSelect
-  export let onNodeClick
+  export let onNodeSelect;
+  export let onNodeClick;
+
+  const dataSourceStore = memo(datasource);
+  const treeOptions = memo({});
+
+  $: dataSourceStore.set(datasource);
 
   let groupByValues = new Set();
-  let rootNodes = []
-  let selectedNodes = new writable([])
-  let loadedDatasource
-  let query
-  let defaultQuery
+  let rootNodes = [];
+  let selectedNodes = new writable([]);
+  let query = {};
+  let defaultQuery;
 
-  $: defaultQuery = LuceneUtils.buildLuceneQuery(filter)
-  $: query = extendQuery(defaultQuery, {})
+  $: defaultQuery = QueryUtils.buildQuery(filter);
+  $: query = extendQuery(defaultQuery, {});
 
   // Fetch data and refresh when needed
-  $: fetch = createFetch( datasource )
+  $: fetch = createFetch($dataSourceStore);
   $: fetch.update({
     query,
     sortColumn,
     sortOrder,
     limit,
     paginate,
-  })
+  });
 
-  $: definition = $fetch?.definition
-  $: primaryDisplay = definition?.primaryDisplay
-  $: getRootNodes ($fetch?.rows, groupNodeLabel)
+  $: definition = $fetch?.definition;
+  $: primaryDisplay = definition?.primaryDisplay;
+  $: getRootNodes($fetch?.rows, groupNodeLabel);
   $: actions = [
     {
       type: ActionTypes.RefreshDatasource,
-      callback: () => fetch.refresh()
-    }]
+      callback: () => fetch.refresh(),
+    },
+  ];
 
-  const createFetch = datasource => {
-    if ( loadedDatasource != datasource ) {
-      loadedDatasource = datasource
-      return fetchData({
-        API,
-        datasource,
-        options: {
-          query,
-          sortColumn,
-          sortOrder,
-          limit,
-          paginate,
-        },
-      })
-    } else {
-      return fetch
-    }
-  }
+  const createFetch = (datasource) => {
+    return fetchData({
+      API,
+      datasource,
+      options: {
+        query,
+        sortColumn,
+        sortOrder,
+        limit,
+        paginate,
+      },
+    });
+  };
 
   const extendQuery = (defaultQuery, extensions) => {
-    const extensionValues = Object.values(extensions || {})
-    let extendedQuery = { ...defaultQuery }
-    extensionValues.forEach(extension => {
+    const extensionValues = Object.values(extensions || {});
+    let extendedQuery = { ...defaultQuery };
+    extensionValues.forEach((extension) => {
       Object.entries(extension || {}).forEach(([operator, fields]) => {
         extendedQuery[operator] = {
           ...extendedQuery[operator],
           ...fields,
-        }
-      })
-    })
-    return extendedQuery
-  }
+        };
+      });
+    });
+    return extendedQuery;
+  };
 
-  const getRootNodes = rows => {
-    let nodes = []
-    groupByValues.clear()
-    rootNodes = []
+  const getRootNodes = (rows) => {
+    let nodes = [];
+    groupByValues.clear();
+    rootNodes = [];
 
-    if ( source == "custom" ) {
-      rootNodes = customNodes.map( ( node, idx ) => {
-        return {
-          id: node.value, 
-          label:node.label,
-          hasSlot : $component.children,
-          renderSlot: true,
-          icon: nodeIcon, 
-          nodeSelection: nodeSelection,
-          selectedNodes: selectedNodes,
-          children: [],
-          open: $builderStore.inBuilder && $component.children && idx == 0,
-        }
-      })
-    }
-
-    if ( groupBy ) {
-      rows.map( row => { groupByValues.add(row[groupField]) })
-      nodes = [...groupByValues]
-      rootNodes = nodes.map( (x, idx ) => { 
+    if (treeType == "groupBy") {
+      rows.map((row) => {
+        groupByValues.add(row[groupField]);
+      });
+      nodes = [...groupByValues];
+      rootNodes = nodes.map((x, idx) => {
         return {
           type: "group",
           renderSlot: false,
-          hasSlot : $component.children,
+          hasSlot: $component.children,
           nodeSelection: nodeSelection,
           selectedNodes: selectedNodes,
           id: x,
           open: $builderStore.inBuilder && $component.children && idx == 0,
-          icon: groupNodeIcon, 
-          label: processStringSync( groupNodeLabel || "{{ value }}" , { value : x } ),
-          children : getGroupChildNodes(x)} })
-    } else if ( recursive ) { 
-        rows.filter( x => !x[joinField] )
-            .map( ( row, idx, arr ) => { rootNodes = [ ...rootNodes, 
-              { 
-                id: row[valueColumn],
-                renderSlot: true,
-                hasSlot : $component.children,
+          icon: groupNodeIcon,
+          label: processStringSync(groupNodeLabel || "{{ value }}", {
+            value: x,
+          }),
+          children: getGroupChildNodes(x),
+        };
+      });
+    } else if (treeType == "recursive") {
+      rows
+        .filter((x) => !x[joinField])
+        .map((row, idx, arr) => {
+          rootNodes = [
+            ...rootNodes,
+            {
+              id: row[valueColumn],
+              renderSlot: true,
+              hasSlot: $component.children,
+              nodeSelection: nodeSelection,
+              selectedNodes: selectedNodes,
+              icon: nodeIcon,
+              open: $builderStore.inBuilder && $component.children && idx == 0,
+              label: row[labelColumn || primaryDisplay],
+              children: getChildNodes(row, rows),
+            },
+          ];
+        });
+    } else {
+      rows.map((row, idx) => {
+        rootNodes = [
+          ...rootNodes,
+          {
+            id: row[valueColumn],
+            renderSlot: true,
+            hasSlot: $component.children,
+            open: $builderStore.inBuilder && $component.children && idx == 0,
+            label: row[labelColumn || primaryDisplay || valueColumn],
+            children: getChildNodes(row),
+          },
+        ];
+      });
+    }
+  };
+
+  const getGroupChildNodes = (groupValue) => {
+    return $fetch.rows
+      .filter((row) => row[groupField] == groupValue)
+      .map((node, idx) => {
+        return {
+          id: node[valueColumn],
+          icon: nodeIcon,
+          renderSlot: true,
+          quiet,
+          hasSlot: $component.children,
+          open: $builderStore.inBuilder && $component.children && idx == 0,
+          label: node[labelColumn || primaryDisplay],
+          nodeSelection: nodeSelection,
+          selectedNodes: selectedNodes,
+          children: getChildNodes(node),
+        };
+      });
+  };
+
+  const getChildNodes = (row, rows) => {
+    let children = [];
+
+    if (linkFields?.length) {
+      linkFields.forEach((element) => {
+        if (validLinkField(element))
+          children.push({
+            type: "link",
+            renderSlot: false,
+            label: element,
+            quiet,
+            nodeSelection: nodeSelection,
+            selectedNodes: selectedNodes,
+            children: row[element]?.map((link) => {
+              return {
                 nodeSelection: nodeSelection,
                 selectedNodes: selectedNodes,
-                icon: nodeIcon, 
-                open: $builderStore.inBuilder && $component.children && idx == 0,
-                label : row[labelColumn || primaryDisplay],
-                children: getChildNodes(row , rows)
-            }] 
-        })
-    } else {
-      rows.map( (row, idx )=> { rootNodes = [ ...rootNodes, 
-        { 
-          id: row[valueColumn],
-          renderSlot: true,
-          hasSlot : $component.children,
-          nodeSelection: nodeSelection,
-          selectedNodes: selectedNodes,
-          icon: nodeIcon, 
-          open: $builderStore.inBuilder && $component.children && idx == 0,
-          label : row[labelColumn || primaryDisplay],
-          children: getChildNodes(row)
-        }]  
-      })
-    }   
-  }
-
-  const getGroupChildNodes = groupValue => {
-    return $fetch.rows.filter( row => row[groupField] == groupValue ).map ( (node,idx) => {
-      return {
-        id: node[valueColumn],
-        icon: nodeIcon,
-        renderSlot: true,
-        quiet,
-        hasSlot : $component.children,
-        open: $builderStore.inBuilder && $component.children && idx == 0,
-        label: node[labelColumn || primaryDisplay],
-        nodeSelection: nodeSelection,
-        selectedNodes: selectedNodes,
-        children : getChildNodes( node )
-      }
-    })
-  }
-
-  const getChildNodes = ( row, rows ) => {
-    let children = []
-
-    if ( linkFields?.length ) {
-      linkFields.forEach(element => {
-        if (validLinkField(element)) children.push( {
-          type: "link",
-          renderSlot : false,
-          icon: "ri-link",
-          label: element,
-          quiet,
-          nodeSelection: nodeSelection,
-          selectedNodes: selectedNodes,
-          children: row[element]?.map( link => { return { 
-            nodeSelection: nodeSelection, 
-            selectedNodes: selectedNodes,
-            id: link["_id"],
-            quiet,
-            label: link.primaryDisplay } })
-        })
+                id: link["_id"],
+                quiet,
+                label: link.primaryDisplay,
+              };
+            }),
+          });
       });
     }
 
-    if ( recursive && joinField) {
-      rows.filter( x => x[joinField] == row[valueColumn] ).forEach( node => {
-        children.push( {
-          renderSlot : true,
-          icon: nodeIcon,
-          id: node[valueColumn],
-          label: node[labelColumn],
-          quiet,
-          nodeSelection: nodeSelection,
-          selectedNodes: selectedNodes,
-          children: getChildNodes( node, rows )
-          })
-        }
-      )
+    if (treeType == "recursive" && joinField) {
+      rows
+        .filter((x) => x[joinField] == row[valueColumn])
+        .forEach((node) => {
+          children.push({
+            renderSlot: true,
+            icon: nodeIcon,
+            id: node[valueColumn],
+            label: node[labelColumn],
+            quiet,
+            nodeSelection: nodeSelection,
+            selectedNodes: selectedNodes,
+            children: getChildNodes(node, rows),
+          });
+        });
     }
 
-    return children
-  }
+    return children;
+  };
 
-  const validLinkField = field => {
-    return definition.schema[field]?.type == "link"
-  }
+  const validLinkField = (field) => {
+    return definition.schema[field]?.type == "link";
+  };
 
-  const handleNodeSelect = ( e ) => {
-    let index = $selectedNodes.findIndex( x => x.id == e.detail.id )
-    if ( index > -1 ) {
-      $selectedNodes.splice(index,1)
-      $selectedNodes = $selectedNodes
-    } else if ( $selectedNodes.length < maxNodeSelection ) {
-      $selectedNodes = [ ...$selectedNodes, e.detail ]
-    } else if ( maxNodeSelection == 1 ) {
-      $selectedNodes = [ e.detail ]
+  const handleNodeSelect = (e) => {
+    let index = $selectedNodes.findIndex((x) => x.id == e.detail.id);
+    if (index > -1) {
+      $selectedNodes.splice(index, 1);
+      $selectedNodes = $selectedNodes;
+    } else if ($selectedNodes.length < maxNodeSelection) {
+      $selectedNodes = [...$selectedNodes, e.detail];
+    } else if (maxNodeSelection == 1) {
+      $selectedNodes = [e.detail];
     } else {
-      notificationStore.actions.warning("Cannot select more than " + maxNodeSelection + " items")
+      notificationStore.actions.warning(
+        "Cannot select more than " + maxNodeSelection + " items"
+      );
     }
-    onNodeSelect?.( e.detail )
-  }
+    onNodeSelect?.(index < 0 ? e.detail : undefined);
+  };
 
-  const handleNodeClick = ( e ) => {
-    onNodeClick?.( e.detail )
-  }
+  const handleNodeClick = (e) => {
+    onNodeClick?.(e.detail);
+  };
 
+  $: treeOptions.set({
+    nodeIcon,
+    nodeSelection,
+    selectedNodes,
+    checkboxes,
+  });
+  setContext("superTreeOptions", treeOptions);
 </script>
-  <div use:styleable={$component.styles} >
-    <Provider actions={actions} scope={ContextScopes.Global}>
-    <ul class="spectrum-TreeView"
-    class:spectrum-TreeView--quiet={quiet}
-    style:visibility={"visible"}
-    style:height={"auto"}
-    style:overflow-y={"auto"}
+
+<div use:styleable={$component.styles}>
+  <Provider {actions} scope={ContextScopes.Global}>
+    <ul
+      class="spectrum-TreeView"
+      class:spectrum-TreeView--quiet={quiet}
+      style:visibility={"visible"}
+      style:height={"auto"}
+      style:overflow-y={"auto"}
     >
       {#if $fetch.loading && !$fetch.loaded}
-        <Skeleton> Loading </Skeleton>
+        <Skeleton>Loading</Skeleton>
       {:else if $fetch?.loaded && rootNodes.length}
         {#if rootless}
-          {#each [...rootNodes] as {id, icon , label, renderSlot, children, open}, idx}
-            <Tree 
+          {#each [...rootNodes] as { id, icon, label, renderSlot, children, open }, idx}
+            <Tree
               {id}
               {icon}
               {disabled}
-              {nodeSelection}
               hasSlot={$component.children}
               {renderSlot}
-              label={label || "Not Set"} 
+              label={label || "Not Set"}
               {open}
               {children}
-              {selectedNodes}
               on:nodeSelect={handleNodeSelect}
               on:nodeClick={handleNodeClick}
               {quiet}
-              > 
-              <slot/>
+            >
+              <slot />
             </Tree>
           {/each}
         {:else}
@@ -293,29 +315,30 @@
             {disabled}
             hasSlot={$component.children}
             renderSlot={false}
-            label={branchName || $component.name} 
-            children={rootNodes} 
-            open={!rootless && !collapsed || ($builderStore.inBuilder && $component.children)}
+            label={branchName || datasource?.label || $component.name}
+            children={rootNodes}
+            open={!rootless && !collapsed}
             {quiet}
             on:nodeSelect={handleNodeSelect}
             on:nodeClick={handleNodeClick}
-            > 
+          >
             <slot />
           </Tree>
         {/if}
       {:else}
         <li class="spectrum-TreeView-item">
-          <a class="spectrum-TreeView-itemLink"> No Records Found </a>
+          <!-- svelte-ignore a11y-invalid-attribute -->
+          <a href="#" class="spectrum-TreeView-itemLink"> No Records Found </a>
         </li>
       {/if}
     </ul>
-    </Provider>
-  </div>
-
+  </Provider>
+</div>
 
 <style>
   .spectrum-TreeView {
     min-width: 250px;
     margin: unset;
+    overflow-y: auto;
   }
 </style>
