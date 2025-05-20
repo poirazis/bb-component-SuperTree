a84dba990f54156f40a1d1eaf814f9b46cadd991
poirazis <poirazis@gmail.com>
2025-05-20 11:09:15 +0300

    Multi Level Groupping

--- lib/Tree.svelte
+++ lib/Tree.svelte
@@ -25,6 +25,7 @@
   export let flat;
   export let row;
   export let group;
+  export let visible;
 
   let openMenu;
   let menuAnchor;
@@ -48,7 +49,7 @@
 
   $: selectionStore = $treeOptions.selectedNodes;
   $: menuStore = $treeOptions.menuStore;
-  $: selected = $selectionStore.findIndex((x) => x.id == id) > -1;
+  $: selected = $selectionStore.findIndex((x) => x == id) > -1;
 
   $: hasChildren = children?.length;
   $: hasButtons = buttons?.length;
@@ -95,171 +96,175 @@
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-<div
-  class="tree-node"
-  class:is-disabled={disabled}
-  class:is-open={open}
-  class:accordion
-  class:groupBranch
-  style:background-color={bgColor}
->
+{#if visible !== false}
   <div
-    class="tree-node-item"
-    class:hasChildren={hasChildren || renderSlot}
-    class:hasDropMenu={hasDropMenu && rightChevron && !hasCheboxes}
-    class:hasButtons
-    class:hasCheckbox={hasCheboxes}
-    class:rightChevron
+    class="tree-node"
+    class:is-disabled={disabled}
+    class:is-open={open}
     class:accordion
-    style:color
-    class:selected
-    class:disabled
-    class:is-menu-open={openMenu}
-    style:background-color={openMenu && !selected
-      ? "var(--spectrum-global-color-gray-100)"
-      : bgColor}
-    on:click|self={handleClick}
+    class:groupBranch
+    style:background-color={bgColor}
   >
-    {#if !rightChevron}
-      <div
-        class="control-content"
-        class:flat={flat && !hasChildren && !renderSlot}
-      >
-        {#if (hasChildren || renderSlot) && $treeOptions.chevronPosition == "left"}
-          <i
-            class="ri-arrow-right-s-line chevron"
-            class:open
-            on:click={handleClick}
-          >
-          </i>
-        {/if}
-      </div>
-    {/if}
+    <div
+      class="tree-node-item"
+      class:hasChildren={hasChildren || renderSlot}
+      class:hasDropMenu={hasDropMenu && rightChevron && !hasCheboxes}
+      class:hasButtons
+      class:hasCheckbox={hasCheboxes}
+      class:rightChevron
+      class:accordion
+      style:color
+      class:selected
+      class:disabled
+      class:is-menu-open={openMenu}
+      style:background-color={openMenu && !selected
+        ? "var(--spectrum-global-color-gray-100)"
+        : bgColor}
+      on:click|self={handleClick}
+    >
+      {#if !rightChevron}
+        <div
+          class="control-content"
+          class:flat={flat && !hasChildren && !renderSlot}
+        >
+          {#if (hasChildren || renderSlot) && $treeOptions.chevronPosition == "left"}
+            <i
+              class="ri-arrow-right-s-line chevron"
+              class:open
+              on:click={handleClick}
+            >
+            </i>
+          {/if}
+        </div>
+      {/if}
 
-    <div class="left-contents" class:hasButtons>
-      {#if hasCheboxes}
-        {#if selected}
-          <i
-            on:click|preventDefault|stopPropagation={handleSelect}
-            class="ri-checkbox-fill checkbox"
-          />
-        {:else}
-          <i
-            on:click|preventDefault|stopPropagation={handleSelect}
-            class="ri-checkbox-blank-line checkbox"
+      <div class="left-contents" class:hasButtons>
+        {#if hasCheboxes}
+          {#if selected}
+            <i
+              on:click|preventDefault|stopPropagation={handleSelect}
+              class="ri-checkbox-fill checkbox"
+            />
+          {:else}
+            <i
+              on:click|preventDefault|stopPropagation={handleSelect}
+              class="ri-checkbox-blank-line checkbox"
+            />
+          {/if}
+        {/if}
+        {#if hasButtons && !disabled}
+          {#each buttons as { text, icon, disabled, quiet, onClick, type }}
+            <SuperButton
+              size="S"
+              {icon}
+              {text}
+              {quiet}
+              {disabled}
+              fillOnHover={true}
+              {type}
+              on:click={() => {
+                dispatch("nodeAction", {
+                  onClick,
+                  row,
+                  group: groupBranch ? label : group,
+                });
+              }}
+            />
+          {/each}
+        {/if}
+        {#if icon}
+          <i class={icon} class:icon style:color={iconColor} />
+        {/if}
+        {#if hasDropMenu && rightChevron}
+          <SuperButton
+            size="XS"
+            icon={$treeOptions.nodeMenuIcon ?? "ri-more-2-fill"}
+            fillOnHover
+            quiet
+            selected={$menuStore == id}
+            on:click={(e) => {
+              menuAnchor = e.detail;
+              openMenu = !openMenu;
+              $menuStore = openMenu ? id : false;
+            }}
+            text=""
           />
         {/if}
-      {/if}
-      {#if hasButtons && !disabled}
-        {#each buttons as { text, icon, disabled, quiet, onClick, type }}
-          <SuperButton
-            size="S"
-            {icon}
-            {text}
-            {quiet}
-            {disabled}
-            fillOnHover={true}
-            {type}
-            on:click={() => {
-              dispatch("nodeAction", {
-                onClick,
-                row,
-                group: groupBranch ? label : group,
-              });
-            }}
-          />
-        {/each}
-      {/if}
-      {#if icon}
-        <i class={icon} class:icon style:color={iconColor} />
-      {/if}
-      {#if hasDropMenu && rightChevron}
-        <SuperButton
-          size="XS"
-          icon={$treeOptions.nodeMenuIcon ?? "ri-more-2-fill"}
-          fillOnHover
-          quiet
-          selected={$menuStore == id}
+        <div
+          class="label"
+          class:selectable
+          class:branch={type == "linkBranch"}
+          style="z-index: 1;"
+          on:mousedown={selectable ? handleSelect : handleClick}
+        >
+          {label || "Not Set"}
+        </div>
+      </div>
+
+      <!-- The Node Action Menu  -->
+      {#if hasDropMenu && !rightChevron}
+        <i
+          class="ri-more-fill menu-icon"
           on:click={(e) => {
-            menuAnchor = e.detail;
+            menuAnchor = e.target;
             openMenu = !openMenu;
             $menuStore = openMenu ? id : false;
           }}
-          text=""
         />
       {/if}
-      <div
-        class="label"
-        class:selectable
-        class:branch={type == "linkBranch"}
-        style="z-index: 1;"
-        on:mousedown={selectable ? handleSelect : handleClick}
-      >
-        {label || "Not Set"}
-      </div>
-    </div>
 
-    <!-- The Node Action Menu  -->
-    {#if hasDropMenu && !rightChevron}
-      <i
-        class="ri-more-fill menu-icon"
-        on:click={(e) => {
-          menuAnchor = e.target;
-          openMenu = !openMenu;
-          $menuStore = openMenu ? id : false;
-        }}
-      />
-    {/if}
-
-    {#if hasChildren && $treeOptions.chevronPosition == "right"}
-      <i
-        class="ri-arrow-right-s-line chevron accordion"
-        on:mousedown={handleClick}
-        class:open
-      >
-      </i>
-    {/if}
-  </div>
-
-  {#if (children?.length || renderSlot) && open}
-    <div
-      class="tree"
-      style:display={open ? "flex" : "none"}
-      transition:slide={{ duration: 130 }}
-    >
-      {#each children as node}
-        <svelte:self
-          id={node.id}
-          {hasSlot}
-          renderSlot={node.renderSlot}
-          label={node.label}
-          children={node.children}
-          quiet={node.quiet}
-          open={node.open}
-          type={node.type}
-          icon={node.icon}
-          color={node.color}
-          bgColor={node.bgColor}
-          group={node.group}
-          {list}
-          {accordion}
-          row={node.row}
-          on:nodeSelect
-          on:nodeClick
-          on:nodeAction
+      {#if hasChildren && $treeOptions.chevronPosition == "right"}
+        <i
+          class="ri-arrow-right-s-line chevron accordion"
+          on:mousedown={handleClick}
+          class:open
         >
-          <slot />
-        </svelte:self>
-      {/each}
-
-      {#if renderSlot && $treeOptions.hasSlot && (open || children.length == 0)}
-        <Provider data={context} scope={ContextScopes.Local}>
-          <slot />
-        </Provider>
+        </i>
       {/if}
     </div>
-  {/if}
-</div>
+
+    {#if (children?.length || renderSlot) && open}
+      <div
+        class="tree"
+        style:display={open ? "flex" : "none"}
+        transition:slide={{ duration: 230 }}
+      >
+        {#each children as node}
+          <svelte:self
+            id={node.id}
+            {hasSlot}
+            renderSlot={node.renderSlot}
+            label={node.label}
+            children={node.children}
+            quiet={node.quiet}
+            open={node.open}
+            type={node.type}
+            icon={node.icon}
+            iconColor={node.iconColor}
+            color={node.color}
+            bgColor={node.bgColor}
+            group={node.group}
+            {list}
+            {accordion}
+            visible={node.visible}
+            row={node.row}
+            on:nodeSelect
+            on:nodeClick
+            on:nodeAction
+          >
+            <slot />
+          </svelte:self>
+        {/each}
+
+        {#if renderSlot && $treeOptions.hasSlot && (open || children.length == 0)}
+          <Provider data={context} scope={ContextScopes.Local}>
+            <slot />
+          </Provider>
+        {/if}
+      </div>
+    {/if}
+  </div>
+{/if}
 
 {#if hasDropMenu && openMenu}
   <SuperPopover
@@ -284,6 +289,8 @@
           on:click={() => {
             dispatch("nodeAction", {
               onClick,
+              id,
+              label,
               row,
               group: groupBranch ? label : group,
             });
